3bb94c87c8876f4f3e560d0c74b6fb431dbbce30
Jose Valencia <jivpacheco@hotmail.com>
2023-02-03 07:36:20 -0300

    configurando el layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,25 @@
 //import React from 'react';
 
-import { Outlet } from "react-router-dom"
+import { Outlet, Link } from "react-router-dom"
 
 function layout() {
   return (
-    <div>
-        <h1 className="text-6xl font-bold"> CRM React</h1>
-    
+    <div className="md:flex md:min-h-screen">
 
-        <Outlet />
+      <aside className="md:w-1/4 bg-blue-900 px-5 py-10">
+        <h2 className="text-4xl font-black text-center text-white"> CMR - Clientes</h2>
+
+        <nav className="mt-10">
+          <Link className="text-2xl block mt-2 hover:text-blue-300 text-white"to="/">Clientes</Link>
+          <Link className="text-2xl block mt-2 hover:text-blue-300 text-white" to="/clientes/nuevo">Nuevo Cliente</Link>
+        </nav>
+
+      </aside>
+
+      <main className="md:w-3/4  p-10 md:h-screen overflow-scroll">
+      <Outlet />
+      </main>
+
     </div>
   )
 }
