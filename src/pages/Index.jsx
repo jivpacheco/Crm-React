3681e72b12a4c25387dfd5045ceecba77a80e85a
Jose Valencia <jivpacheco@hotmail.com>
2023-02-08 07:31:14 -0300

    configurando componente clientes

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,44 +1,47 @@
 import React, { Fragment } from 'react'
 import { useLoaderData } from 'react-router-dom';
+import Cliente from '../components/Cliente';
 
-export function loader(){
+export function loader() {
   const clientes = [
     {
-        id: 1,
-        nombre: 'Juan',
-        telefono: 102013313,
-        email: "juan@juan.com",
-        empresa: 'Codigo Con Juan'
+      id: 1,
+      nombre: 'Juan',
+      telefono: 102013313,
+      email: "juan@juan.com",
+      empresa: 'Codigo Con Juan'
     },
     {
-        id: 2,
-        nombre: 'Karen',
-        telefono: 138198313,
-        email: "karen@juan.com",
-        empresa: 'Codigo Con Juan'
+      id: 2,
+      nombre: 'Karen',
+      telefono: 138198313,
+      email: "karen@juan.com",
+      empresa: 'Codigo Con Juan'
     },
     {
-        id: 3,
-        nombre: 'Josue',
-        telefono: 31983913,
-        email: "josue@juan.com",
-        empresa: 'Codigo Con Juan'
+      id: 3,
+      nombre: 'Josue',
+      telefono: 31983913,
+      email: "josue@juan.com",
+      empresa: 'Codigo Con Juan'
     },
     {
-        id: 4,
-        nombre: 'Miguel',
-        telefono: 319381983,
-        email: "miguel@juan.com",
-        empresa: 'Codigo Con Juan'
+      id: 4,
+      nombre: 'Miguel',
+      telefono: 319381983,
+      email: "miguel@juan.com",
+      empresa: 'Codigo Con Juan'
     },
     {
-        id: 5,
-        nombre: 'Pedro',
-        telefono: 1398198938,
-        email: "pedro@juan.com",
-        empresa: 'Codigo Con Juan'
+      id: 5,
+      nombre: 'Pedro',
+      telefono: 1398198938,
+      email: "pedro@juan.com",
+      empresa: 'Codigo Con Juan'
     },
-];
+  ];
+
+  const datos = [];
 
   return clientes;
 }
@@ -46,13 +49,49 @@ export function loader(){
 
 function Index() {
 
-  const datos = useLoaderData();
-  console.log(datos)
+  const clientes = useLoaderData();
+  //console.log(datos)
+  //const datos = useLoaderData();
+
 
   return (
     <Fragment>
       <h1 className='text-4xl font-black text-blue-900 '>Clientes</h1>
       <p className='mt-3'>Administra tus Clientes</p>
+
+      {clientes.length ? (
+        <table className='w-full bg-white shadow mt-5 table-auto'>
+          <thead className='bg-blue-800 text-white '>
+            <tr>
+              <th className='p-2'>Cliente</th>
+              <th className='p-2'>Contacto</th>
+              <th className='p-2'>Acciones</th>
+            </tr>
+
+          </thead>
+          <tbody>
+            {clientes.map(cliente => (
+              <Cliente 
+              cliente = {cliente}
+              key = {cliente.id}
+              />
+  
+              // <tr>
+              //   <td className='p-6'>
+              //      {clientes.nombre}
+              //   </td>
+              //   <td className='p-6'>
+              //      {clientes.telefono}
+              //   </td>
+              // </tr>
+            ))}
+          </tbody>
+
+        </table>
+
+      ) : (
+        <p className='text-center mt-10'>No Hay Clientes Aún</p>
+      )}
     </Fragment>
   )
 }
